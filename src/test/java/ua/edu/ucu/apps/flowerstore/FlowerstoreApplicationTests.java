440911eeb30384f27557c844eb56756625450fe6
LidaSemsichko <semsichko.pn@ucu.edu.ua>
2024-10-28 19:11:06 +0300

    changes6

--- src/test/java/ua/edu/ucu/apps/flowerstore/FlowerstoreApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/flowerstore/FlowerstoreApplicationTests.java
@@ -2,12 +2,9 @@ package ua.edu.ucu.apps.flowerstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class FlowerstoreApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
